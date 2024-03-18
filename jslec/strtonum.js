2324d36561909a9ed34ad87bb7bc710a582700d3
Somay Lal Pawa <93252032+SomayPawa@users.noreply.github.com>
2024-03-18 18:19:13 +0000

    basic change

--- jslec/strtonum.js
+++ jslec/strtonum.js
@@ -1,1 +1,8 @@
-console.log("hello");
+let str1 = "somay";
+let str2 = " lal";
+console.log(str1+str2);
+
+// interesting part 
+console.log("1"+2+3); // "According to me, I expected 15, but I got 123! This is amazing!"
+
+console.log(1+2+"3"); // "Now the output has changed; I got 33, but I expected 123."
