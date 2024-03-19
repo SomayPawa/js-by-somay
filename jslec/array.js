f467ce96e6358c6b248d8aa84f7efe69e8b7d67f
Somay Lal Pawa <93252032+SomayPawa@users.noreply.github.com>
2024-03-19 16:14:38 +0000

    array concept

---
+++ jslec/array.js
@@ -0,0 +1,23 @@
+console.log("jai maata di");
+
+let arr1 = [1,2,3,4];
+let arr2 = [5,6,7,8];
+
+// arr1.push(arr2);
+// console.log(arr1);
+
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+//i expected a simple array as the output, but i received an array nested within another array,
+
+
+// now important one is spread one
+// let arr4 = [...arr1,...arr2];
+// console.log(arr4);
+
+
+// now chnage something like name into array
+
+let name = "somaylalpawa";
+console.log(Array.from(name));
