ddf4a214cac632aff71ce7cce992fdaa844dc8c0
Somay Lal Pawa <93252032+SomayPawa@users.noreply.github.com>
2024-03-19 06:52:54 +0000

    math and number concept

---
+++ jslec/number.js
@@ -0,0 +1,13 @@
+let number = 100;
+console.log("the number is",number.toFixed(2));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1)
+
+
+// like i want value between max and min then 
+
+const max = 20;
+const min = 10;
+
+console.log((Math.floor(Math.random()*(max-min+1)))+min);
